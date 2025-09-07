0406f351c00846682daa5380067205737a6a2f80
libsyfer <libsyfer@gmail.com>
2025-09-07 21:44:30 +0300

    Реализован сервис клинта тунеля

---
+++ src/NetTunnel.Client/ClientSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace NetTunnel.Client
+{
+    public class ClientSettings
+    {
+        public const string Section = "ClientSettings";
+        public int ListenPort { get; set; }
+
+        public string ServerIp { get; set; }
+
+        public int ServerPort { get; set; }
+
+        public string PreSharedKey { get; set; }
+
+        public IPAddress GetServerIp => IPAddress.Parse(ServerIp);
+    }
+}
--- src/NetTunnel.Client/Program.cs
+++ src/NetTunnel.Client/Program.cs
@@ -1,7 +1,24 @@
+using Microsoft.Extensions.Options;
 using NetTunnel.Client;
+using NetTunnel.Core;
+using System.Net;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+builder.Services.Configure<ClientSettings>(builder.Configuration.GetSection(ClientSettings.Section));
+builder.Services.AddSingleton(sp =>
+{
+    var logger = sp.GetRequiredService<ILogger<UdpTunnelClient>>();
+    var clientSettings = sp.GetRequiredService<IOptions<ClientSettings>>().Value;
+
+    var ip = clientSettings.GetServerIp;
+
+    return new UdpTunnelClient(logger, clientSettings.ListenPort,
+        new IPEndPoint(clientSettings.GetServerIp, clientSettings.ServerPort),
+        clientSettings.PreSharedKey);
+});
+
+builder.Services.AddHostedService<ClientHostedService>();
 
 var host = builder.Build();
 host.Run();
