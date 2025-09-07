715a42f2b364de40fe82b11ae021691dc33b4333
libsyfer <libsyfer@gmail.com>
2025-09-07 22:40:42 +0300

    Реализован сервис сервера туннеля

---
+++ src/NetTunnel.Server/ServerHostedService.cs
@@ -0,0 +1,30 @@
+﻿using NetTunnel.Core;
+
+namespace NetTunnel.Server
+{
+    public class ServerHostedService : IHostedService
+    {
+        private readonly ILogger<ServerHostedService> _logger;
+        private readonly UdpTunnelServer _server;
+
+        public ServerHostedService(ILogger<ServerHostedService> logger, UdpTunnelServer server)
+        {
+            _logger = logger;
+            _server = server;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Start udp tunnel server");
+
+            await _server.StartAsync(cancellationToken);
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Stop udp tunnel server");
+
+            await _server.StopAsync();
+        }
+    }
+}
---
+++ src/NetTunnel.Server/ServerSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace NetTunnel.Server
+{
+    public class ServerSettings
+    {
+        public const string Section = "ServerSettings";
+        public string ListenIp { get; set; } = string.Empty;
+        public int ListenPort { get; set; }
+        public int TargetPort { get; set; }
+        public string PreSharedKey { get; set; } = string.Empty;
+        public IPAddress GetListenIp => IPAddress.Parse(ListenIp);
+    }
+}
