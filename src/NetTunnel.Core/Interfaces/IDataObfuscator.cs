778c4eabf4bd455d7a0d7f2276236e8bc3ba7805
libsyfer <libsyfer@gmail.com>
2025-09-10 01:44:52 +0300

    Созданы интерфейсы обфускации и подписи данных

---
+++ src/NetTunnel.Core/Interfaces/IDataObfuscator.cs
@@ -0,0 +1,8 @@
+﻿namespace NetTunnel.Core.Interfaces
+{
+    internal interface IDataObfuscator
+    {
+        byte[] Obfuscate(byte[] data);
+        byte[] Deobfuscate(byte[] data);
+    }
+}
---
+++ src/NetTunnel.Core/Interfaces/IDataSigner.cs
@@ -0,0 +1,8 @@
+﻿namespace NetTunnel.Core.Interfaces
+{
+    internal interface IDataSigner
+    {
+        byte[] CreateSignature(byte[] data);
+        bool VerifySignature(byte[] data, byte[] signature);
+    }
+}
