e1280e5838f96bc77ec7432764e69d4d8ef392eb
libsyfer <libsyfer@gmail.com>
2025-09-10 15:28:17 +0300

    Создан интерфейс билдера пакетов туннеля

---
+++ src/Core/NetTunnel.Domain/Interfaces/ITunnelPacketBuilder.cs
@@ -0,0 +1,8 @@
+﻿namespace NetTunnel.Domain.Interfaces
+{
+    public interface ITunnelPacketBuilder<TPacket> where TPacket : class
+    {
+        byte[] BuildPacket(TPacket packet);
+        TPacket ParsePacket(byte[] data);
+    }
+}
