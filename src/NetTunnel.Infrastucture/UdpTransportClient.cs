ac249caa28f5777bb568d882011f2987dd8a0727
libsyfer <libsyfer@gmail.com>
2025-09-11 02:13:55 +0300

    Реализован класс транспортного клиента Udp

---
+++ src/NetTunnel.Infrastucture/UdpTransportClient.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+using NetTunnel.Domain.Entities;
+using NetTunnel.Domain.Interfaces;
+using System.Net;
+using System.Net.Sockets;
+
+namespace NetTunnel.Infrastucture
+{
+    public class UdpTransportClient : ITransportClient
+    {
+        private readonly ILogger<UdpTransportClient> _logger;
+        private readonly UdpClient _client;
+
+        public UdpTransportClient(ILogger<UdpTransportClient> logger,
+            IPEndPoint listenEndpoint)
+        {
+            _logger = logger;
+            _client = new UdpClient(listenEndpoint);
+        }
+
+        public IPEndPoint? EndPoint => (IPEndPoint?)_client.Client.LocalEndPoint;
+
+        public async Task<TransportClientResult> ReceiveAsync(CancellationToken cancellationToken)
+        {
+            var udpReceiveResult = await _client.ReceiveAsync(cancellationToken);
+            _logger.LogDebug("Receive {BytesLength}bytes from {RemoteEndpoint}", udpReceiveResult.Buffer, udpReceiveResult.RemoteEndPoint);
+
+            return new TransportClientResult
+            {
+                Data = udpReceiveResult.Buffer,
+                RemoteEndPoint = udpReceiveResult.RemoteEndPoint,
+            };
+        }
+
+        public async Task<int> SendAsync(ReadOnlyMemory<byte> data, IPEndPoint endPoint, CancellationToken cancellationToken)
+        {
+            _logger.LogDebug("Send {BytesLength}bytes to {TargetEndpoint}", data.Length, endPoint);
+            return await _client.SendAsync(data, endPoint, cancellationToken);
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+    }
+}
