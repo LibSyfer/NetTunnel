b2d03ddb275ca290363b308337319a00acba3a2a
libsyfer <libsyfer@gmail.com>
2025-09-10 16:19:00 +0300

    Создан класс дефолтного пакета туннеля

---
+++ src/Core/NetTunnel.Domain/Entities/DefaultTunnelPacket.cs
@@ -0,0 +1,9 @@
+﻿namespace NetTunnel.Domain.Entities
+{
+    public class DefaultTunnelPacket
+    {
+        public byte[]? Data { get; set; }
+
+        public byte[]? Sign { get; set; }
+    }
+}
