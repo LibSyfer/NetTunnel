bf1982c3a5e32781d48fdd8d67b032ca220ddc58
libsyfer <libsyfer@gmail.com>
2025-09-11 02:12:43 +0300

    Создан интерфейс транспортного клиента

---
+++ src/NetTunnel.Domain/Entities/TransportClientResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace NetTunnel.Domain.Entities
+{
+    public struct TransportClientResult
+    {
+        public byte[] Data { get; set; }
+        public IPEndPoint RemoteEndPoint { get; set; }
+    }
+}
---
+++ src/NetTunnel.Domain/Interfaces/ITransportClient.cs
@@ -0,0 +1,16 @@
+﻿using NetTunnel.Domain.Entities;
+using System.Net;
+
+namespace NetTunnel.Domain.Interfaces
+{
+    public interface ITransportClient : IDisposable
+    {
+        IPEndPoint? EndPoint { get; }
+        Task<TransportClientResult> ReceiveAsync(CancellationToken cancellationToken);
+        Task<int> SendAsync(ReadOnlyMemory<byte> data, IPEndPoint endPoint, CancellationToken cancellationToken);
+    }
+
+    public interface IExternalTransportClient : ITransportClient;
+
+    public interface ITunnelTransportClient : ITransportClient;
+}
