3ca3f0535a72cf6c8f580bcf70ada9f8c4ec23fb
libsyfer <libsyfer@gmail.com>
2025-09-10 13:55:05 +0300

    Реализован класс xor обфускатора

---
+++ src/NetTunnel.Infrastucture/Security/XorDataObfuscator.cs
@@ -0,0 +1,32 @@
+﻿using NetTunnel.Domain.Interfaces;
+
+namespace NetTunnel.Infrastucture.Security
+{
+    internal class XorDataObfuscator : IDataObfuscator
+    {
+        private readonly byte[] _key;
+
+        public XorDataObfuscator(byte[] key)
+        {
+            _key = key ?? throw new ArgumentNullException(nameof(key));
+        }
+
+        public byte[] Deobfuscate(byte[] data) => XorData(data);
+
+        public byte[] Obfuscate(byte[] data) => XorData(data); 
+
+        private byte[] XorData(byte[] data)
+        {
+            if (data == null || data.Length == 0)
+                throw new ArgumentException("Data cannot be null or empty");
+
+            byte[] result = new byte[data.Length];
+            for (int i = 0; i < data.Length; ++i)
+            {
+                result[i] = (byte)(data[i] ^ _key[i % _key.Length]);
+            }
+
+            return result;
+        }
+    }
+}
