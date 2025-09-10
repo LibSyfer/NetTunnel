339e70267c1620737c64f78a4657fae6aa1a45b4
libsyfer <libsyfer@gmail.com>
2025-09-10 13:54:21 +0300

    Созданы интерфейсы обфускаторов и подписчиков данных

---
+++ src/Core/NetTunnel.Domain/Interfaces/IDataSigner.cs
@@ -0,0 +1,8 @@
+﻿namespace NetTunnel.Domain.Interfaces
+{
+    public interface IDataSigner
+    {
+        byte[] CreateSignature(byte[] data);
+        bool VerifySignature(byte[] data, byte[] signature);
+    }
+}
