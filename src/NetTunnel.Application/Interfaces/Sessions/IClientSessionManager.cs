b0cfe4f2fefda7892ffb5b7a784430026fdfc9ba
libsyfer <libsyfer@gmail.com>
2025-09-13 01:18:02 +0300

    Создан интерфейс менеджера сессий

---
+++ src/NetTunnel.Application/Interfaces/Sessions/IClientSessionManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace NetTunnel.Application.Interfaces.Sessions
+{
+    public interface IClientSessionManager
+    {
+        Task<int> SendAsync(ReadOnlyMemory<byte> data, IPEndPoint endPoint, CancellationToken cancellationToken);
+        void CleanupInactiveSessions();
+    }
+}
