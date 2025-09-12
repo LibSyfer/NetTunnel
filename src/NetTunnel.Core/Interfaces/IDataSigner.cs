8f2e847eed67135fc5f3ec1abc396ce7462da817
libsyfer <libsyfer@gmail.com>
2025-09-12 15:23:56 +0300

    Удалены старые проекты

--- src/NetTunnel.Core/Interfaces/IDataSigner.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NetTunnel.Core.Interfaces
-{
-    internal interface IDataSigner
-    {
-        byte[] CreateSignature(byte[] data);
-        bool VerifySignature(byte[] data, byte[] signature);
-    }
-}
