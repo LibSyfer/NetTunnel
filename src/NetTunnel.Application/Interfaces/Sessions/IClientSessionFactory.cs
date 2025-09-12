21cc4c9e5ecb05b29fd97ff1f97dc11900b17e6a
libsyfer <libsyfer@gmail.com>
2025-09-13 02:34:00 +0300

    Удален интерфейс фабрики сессий и его реализация

--- src/NetTunnel.Application/Interfaces/Sessions/IClientSessionFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Net;
-
-namespace NetTunnel.Application.Interfaces.Sessions
-{
-    public interface IClientSessionFactory
-    {
-        IClientSession CreateSession(ITransportClient _replyClient, IPEndPoint replyEndpoint);
-    }
-}
