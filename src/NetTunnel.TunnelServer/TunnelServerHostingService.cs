f7007b68c49578b482c876a3241ca0ba5b8fd45a
libsyfer <libsyfer@gmail.com>
2025-09-12 15:30:58 +0300

    Реаслизован сервис сервера туннеля

---
+++ src/NetTunnel.TunnelServer/TunnelServerHostingService.cs
@@ -0,0 +1,30 @@
+﻿using NetTunnel.Application.Interfaces;
+
+namespace NetTunnel.TunnelServer
+{
+    internal class TunnelServerHostingService : IHostedService
+    {
+        private readonly ILogger<TunnelServerHostingService> _logger;
+        private readonly ITunnelNode _client;
+
+        public TunnelServerHostingService(ILogger<TunnelServerHostingService> logger, ITunnelNode client)
+        {
+            _logger = logger;
+            _client = client;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Start tunnel client");
+
+            await _client.StartAsync(cancellationToken);
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Stop tunnel client");
+
+            await _client.StopAsync(cancellationToken);
+        }
+    }
+}
