66e981864b83bf44dc3485030da1f1ac90434f55
libsyfer <libsyfer@gmail.com>
2025-09-12 22:56:03 +0300

    Удалены ненужные интерфейсы

--- src/NetTunnel.Application/Interfaces/IExternalClientSession.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NetTunnel.Application.Interfaces
-{
-    public interface IExternalClientSession<TTransportClient> : IDisposable
-        where TTransportClient : class
-    {
-        Task StartAsync(CancellationToken cancellationToken = default);
-        Task StopAsync(CancellationToken cancellationToken = default);
-    }
-}
--- src/NetTunnel.Application/Interfaces/IExternalClientSessionFactory.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace NetTunnel.Application.Interfaces
-{
-    public interface IExternalClientSessionFactory
-    {
-    }
-}
