b77b3ee170603bb120ffb27a4b5d88f8dbbc560a
libsyfer <libsyfer@gmail.com>
2025-09-21 17:57:38 +0300

    Добавлена поддердка доменных имен в конфигурации генератора пакетов udp

--- src/NetTunnel.UdpTrafficGenerator/TraficGeneratorSettings.cs
+++ src/NetTunnel.UdpTrafficGenerator/TraficGeneratorSettings.cs
@@ -5,10 +5,12 @@ namespace NetTunnel.UdpTrafficGenerator
     internal class TraficGeneratorSettings
     {
         public const string Section = "TraficGeneratorSettings";
-        public string TargetIp { get; set; } = string.Empty;
+        public string TargetHost { get; set; } = string.Empty;
         public int TargetPort { get; set; }
         public int SendingDelayMs { get; set; }
         public string SendingMessage { get; set; } = string.Empty;
-        public IPAddress GetTargetIp => IPAddress.Parse(TargetIp);
+        public IPAddress GetTargetIp =>
+            Dns.GetHostEntry(TargetHost).AddressList.FirstOrDefault() ??
+            throw new InvalidOperationException($"Could not resolve IP address for host: {TargetHost}");
     }
 }
