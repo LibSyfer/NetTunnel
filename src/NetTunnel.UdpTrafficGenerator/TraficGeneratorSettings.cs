5fbef75b90673db3ba6261a39c6c5ea331b56ba1
libsyfer <libsyfer@gmail.com>
2025-09-21 18:23:50 +0300

    Добавлена конфигурация udp клинета для отправки и прослушиавния обратных сообщений

--- src/NetTunnel.UdpTrafficGenerator/TraficGeneratorSettings.cs
+++ src/NetTunnel.UdpTrafficGenerator/TraficGeneratorSettings.cs
@@ -5,10 +5,14 @@ namespace NetTunnel.UdpTrafficGenerator
     internal class TraficGeneratorSettings
     {
         public const string Section = "TraficGeneratorSettings";
+        public string ListenIp { get; set; } = string.Empty;
+        public int ListenPort { get; set; }
         public string TargetHost { get; set; } = string.Empty;
         public int TargetPort { get; set; }
         public int SendingDelayMs { get; set; }
         public string SendingMessage { get; set; } = string.Empty;
+
+        public IPAddress GetListenIpp => IPAddress.Parse(ListenIp);
         public IPAddress GetTargetIp =>
             Dns.GetHostEntry(TargetHost).AddressList.FirstOrDefault() ??
             throw new InvalidOperationException($"Could not resolve IP address for host: {TargetHost}");
